0cb5badcd57def15dbdb67e4f906a3096f727dbd
James Mugliston <james.mugliston@gmail.com>
2023-12-08 11:59:38 +0000

    2023 day-08

---
+++ src/challenges/2023/day-08/solution/solution.test.js
@@ -0,0 +1,23 @@
+import test from "node:test";
+import assert from "assert";
+import { join } from "path";
+import { readFileSync } from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+import { part1, part2 } from "./solution.js";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const input = readFileSync(join(__dirname, "../example.txt"), "utf8");
+const input2 = readFileSync(join(__dirname, "../example2.txt"), "utf8");
+const input3 = readFileSync(join(__dirname, "../example3.txt"), "utf8");
+
+test("2023 | day 08 | part 1", () => {
+  assert.equal(part1(input), 2);
+  assert.equal(part1(input2), 6);
+});
+
+test("2023 | day 08 | part 2", () => {
+  assert.equal(part2(input3), 6);
+});
