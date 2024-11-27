64122dcba048fb3cbe9579c6187859b8a0c6c909
James Mugliston <james.mugliston@gmail.com>
2024-11-27 20:24:19 +0000

    2022 day-13 improve readability

--- src/challenges/2022/day-13/solution/solution.js
+++ src/challenges/2022/day-13/solution/solution.js
@@ -1,37 +1,36 @@
 import { max } from "../../../../helpers.js";
 
-const sortPackets = (packet1, packet2) => {
+const parseInput = (input) =>
+  input
+    .trim()
+    .split("\n\n")
+    .map((x) => x.split("\n").map((x) => JSON.parse(x.trim())));
+
+const packetComparator = (packet1, packet2) => {
   if (Number.isInteger(packet1) && Number.isInteger(packet1)) {
     if (packet1 < packet2) {
       return -1;
     }
-    if (packet1 > packet2) {
-      return 1;
-    }
     if (packet1 === packet2) {
       return 0;
     }
+    if (packet1 > packet2) {
+      return 1;
+    }
   }
 
-  if (Array.isArray(packet1) && Number.isInteger(packet2)) {
-    packet2 = [packet2];
-  }
+  packet1 = Array.isArray(packet1) ? packet1 : [packet1];
+  packet2 = Array.isArray(packet2) ? packet2 : [packet2];
 
-  if (Array.isArray(packet2) && Number.isInteger(packet1)) {
-    packet1 = [packet1];
-  }
-
-  let sorted = 0;
+  let sorted;
   for (let i = 0; i < max([packet1.length, packet2.length]); i++) {
     if (packet1.length - 1 < i) {
       return -1;
     }
-
     if (packet2.length - 1 < i) {
       return 1;
     }
-
-    sorted = sortPackets(packet1[i], packet2[i]);
+    sorted = packetComparator(packet1[i], packet2[i]);
     if (sorted === -1) {
       return -1;
     } else if (sorted === 1) {
@@ -43,36 +42,28 @@ const sortPackets = (packet1, packet2) => {
 };
 
 const part1 = (input) => {
-  const packets = input
-    .trim()
-    .split("\n\n")
-    .map((x) => x.split("\n").map((x) => JSON.parse(x.trim())));
+  const packets = parseInput(input);
 
   let sumCorrectIndexes = 0;
-  let index = 1;
-  for (const [packet1, packet2] of packets) {
-    if (sortPackets(packet1, packet2) === -1) {
-      sumCorrectIndexes = sumCorrectIndexes + index;
+  for (const [index, [packet1, packet2]] of packets.entries()) {
+    if (packetComparator(packet1, packet2) === -1) {
+      sumCorrectIndexes += index + 1;
     }
-    index += 1;
   }
 
   return sumCorrectIndexes;
 };
 
 const part2 = (input) => {
-  let packets = input
-    .trim()
-    .split("\n\n")
-    .map((x) => x.split("\n").map((x) => JSON.parse(x.trim())))
-    .reduce((acc, curr) => [...acc, curr[0], curr[1]], []);
-
-  packets.push([[2]]);
-  packets.push([[6]]);
+  const packets = [
+    ...parseInput(input).reduce((acc, curr) => [...acc, ...curr], []),
+    [[2]], // These are the "divider" packets
+    [[6]],
+  ];
 
   const sortedPackets = packets
-    .sort((a, b) => sortPackets(a, b))
-    .map((x) => JSON.stringify(x));
+    .sort((a, b) => packetComparator(a, b))
+    .map((packet) => JSON.stringify(packet));
 
   return (
     (sortedPackets.indexOf("[[2]]") + 1) * (sortedPackets.indexOf("[[6]]") + 1)
